b3a19909913367e958dbc54892dbf470ceb6fb56
harry-hdh <hdh997@outlook.com>
2026-04-24 00:27:49 +1200

    Added truncate, restart_sequence, create_cur funcs for main

---
+++ src/project_03/utils.py
@@ -0,0 +1,60 @@
+import psycopg2
+import io
+from config import load_config
+
+def create_cur():
+    config = load_config()
+    conn = psycopg2.connect(**config)
+    cur = conn.cursor()
+    return cur,conn
+
+def fetch_sequence_name(table_name, column_name):
+    # pg_get_serial_sequence returns the sequence name in 'schema.sequence' format
+    command = "SELECT pg_get_serial_sequence(%s, %s);"
+
+    try:
+        cur, conn = create_cur()
+        cur.execute(command, (table_name, column_name))
+
+        result = cur.fetchone()
+
+        cur.close()
+        conn.close()
+
+        # If a sequence exists, it returns a string; otherwise, it returns None
+        if result and result[0]:
+            return result[0]
+        return False
+
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(f"Error fetching sequence for {table_name}: {error}")
+        return False
+
+def truncate_table(table_name):
+    try:
+        sql = f"TRUNCATE {table_name} CASCADE;"
+        cur, conn = create_cur()
+        cur.execute(sql)
+        conn.commit()
+        cur.close()
+        conn.close()
+        print(f'Truncated {table_name}.')
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def restart_sequence(table_name, column_name):
+    sequence = fetch_sequence_name(table_name,column_name)
+    command = f"ALTER SEQUENCE {sequence} RESTART;"
+    try:
+        cur, conn = create_cur()
+        cur.execute(command)
+        conn.commit()
+        cur.close()
+        conn.close()
+        print(f'Restarted sequence for {table_name}.')
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(f"Error restart sequence for {table_name}: {error}")
+        return False
+
+#truncate_table('promotion_product')
+#print(fetch_sequence_name('promotion_product', 'promo_product_id'))
